aedb63aad42cdeb7ed7124450400583f7026154a
Daryl Occ <darylnator@gmail.com>
2017-03-12 16:15:10 -0700

    added isWinner method and other methods for minmax

--- AdverserialSearch.java
+++ AdverserialSearch.java
@@ -4,13 +4,14 @@ import java.util.*;
 
 public class AdverserialSearch {
 	static final int DIMENSION = 9, MAX = 30;
-	static String player;
+	static String opponent = " O";
+;
 	static String[][] board = 
 		{
 				{" ", " 1", " 2", " 3", " 4", " 5", " 6", " 7", " 8"},
-				{"A", " -", " -", " -", " -", " -", " -", " -", " -"},
+				{"A", " X", " X", " X", " -", " -", " -", " -", " -"},
 				{"B", " -", " -", " -", " -", " -", " -", " -", " -"},
-				{"C", " -", " -", " -", " -", " -", " -", " -", " -"},
+				{"C", " O", " O", " O", " -", " -", " -", " -", " -"},
 				{"D", " -", " -", " -", " -", " -", " -", " -", " -"},
 				{"E", " -", " -", " -", " -", " -", " -", " -", " -"},
 				{"F", " -", " -", " -", " -", " -", " -", " -", " -"},
@@ -29,25 +30,37 @@ public class AdverserialSearch {
 	    		if (timeLimit <= MAX){
 	    			System.out.print("\nWould you like to go first?(y or n): ");
 	    			while (true) {
-	    				String answer = cin.next();
+	    				Node state = new Node();
+	    				String answer = cin.next(), move;
 		    			if (answer.equals("y")) {
-		    				player = "opponent";
-		    				System.out.println();
-		    				printBoard(board);
-		    		    	System.out.print("\nMy current move is: ");
-		    		    	String move = cin.next();
-		    		    	markMoveOnBoard(move);
-		    		    	miniMaxAlgorithm(move);
+		    				while (true) {
+		    					System.out.println();
+			    				printBoard(board);
+			    		    	System.out.print("\nMy current move is: ");
+			    		    	move = cin.next();
+			    		    	markOponentMoveOnBoard(move);
+			    		    	if (isWinner(opponent, move)) break;
+	//		    		    	miniMaxAlgorithm(state);
+//			    		    	System.out.print("\nChoose your next move: " + state.printMove());
+			    		    	if (isWinner(state.getPlayer(), state.getPlayer())) break;
+		    				}
+		    				System.out.println("\nOpponent wins");
 		    				break;
 		    			}else if (answer.equals("n")) {
-		    				player = "computer";
-		    				System.out.println();
-		    				printBoard(board);
-		    		    	System.out.print("\nMy current move is: " + markRandMoveOnBoard());
-//		    		    	miniMaxAlgorithm(move);
+		    				while (true) {
+		    					System.out.println();
+			    				printBoard(board);
+   	//		    		    	miniMaxAlgorithm(state);
+//			    		    	System.out.print("\nMy current move is: " + state.printMove());
+			    		    	if (isWinner(state.getPlayer(), state.getPlayer())) break;
+			    		    	System.out.println("\nChoose your next move: ");
+			    		    	move = cin.next();
+			    		    	markOponentMoveOnBoard(move);
+			    		    	if (isWinner(state.getPlayer(), move)) break;
+//			    				state.setBoard(board);
+		    				}
 		    		    	break;
 		    			}else{System.out.print("\nPlease enter either \"y\" or \"n\": ");}
-
 					}
 	    			break;
 	    		} else {System.out.print("\nPlease enter a number in between or including 1 and 30: ");}
@@ -65,12 +78,47 @@ public class AdverserialSearch {
 		}
 	}
 	
-	public static void miniMaxAlgorithm(String move) {
-		// TODO Auto-generated method stub
-
+	public static void miniMaxAlgorithm(Node state) {
+		state = minValue();
 	}
 	
-	public static void markMoveOnBoard(String move) {
+	public static Node minValue() {
+		return null;
+	}
+	
+	public static Node maxValue() {
+		return null;
+	}
+	
+	public static boolean terminalTest() {
+		return true;
+	}
+	
+	public static int utility() {
+		return 0;
+	}
+	
+	public static boolean isWinner(String player, String move) {
+		int row = move.charAt(0) - 'a' + 1;
+		int col = Integer.parseInt(String.valueOf(move.charAt(1)));
+		int count = 0;
+
+		for (int i = 1; i < DIMENSION; i++) {
+			if (board[row][i].equals(player)) count++;
+			else count = 0;
+			
+			if (count == 4) return true;
+		}
+		for (int i = 1; i < DIMENSION; i++) {
+			if (board[i][col].equals(player)) count++;
+			else count = 0;
+			
+			if (count == 4) return true;
+		}
+		return false;
+	}
+	
+	public static void markOponentMoveOnBoard(String move) {
 		String letter = String.valueOf(move.charAt(0));
 		String number = String.valueOf(move.charAt(1));
 		
@@ -89,6 +137,7 @@ public class AdverserialSearch {
 		int i1 = letter.charAt(0) - 'a' + 1;
 		int i2 = Integer.parseInt(number);
 		board[i1][i2] = board[i1][i2].replace("-", "X");
+		System.out.println();
 		printBoard(board);
 	}
 	
@@ -96,13 +145,15 @@ public class AdverserialSearch {
 		int i1 = letter.charAt(0) - 'a' + 1;
 		int i2 = Integer.parseInt(number);
 		board[i1][i2] = board[i1][i2].replace("-", "O");
+		System.out.println();
 		printBoard(board);
 	}
 }
 
 class Node {
+	String player;
 	private int value;
-	String [][] newCurrentBoard = new String[9][9];
+	String [][] newCurrentBoard = new String[5][5];
 	Queue<Node> successorStates = new PriorityQueue<>();
 	
 	Node(){}
@@ -111,9 +162,28 @@ class Node {
 		System.arraycopy(currentBoard, 0, newCurrentBoard, 0, currentBoard.length);
 	}
 	
+	Node (String[][] currentBoard, String player) {
+		System.arraycopy(currentBoard, 0, newCurrentBoard, 0, currentBoard.length);
+		this.player = player;
+	}
+	
+	public void setBoard(String[][] newCurrentBoard) {
+		System.arraycopy(newCurrentBoard, 0, this.newCurrentBoard, 0, newCurrentBoard.length);
+	}
+	
 	public void setValue(int value) {this.value = value;}
 	
+	public void setPlayer(String player) {this.player = player;}
+	
+	public String[][] getBoard() {return newCurrentBoard;}
+	
 	public int getValue() {return value;}
+	
+	public String getPlayer() {return player;}
+	
+	public void printMove() {
+		//print move by using array indeces that position "X"
+	}
 }
 
 class SuccessorComparator implements Comparator<Node> {
